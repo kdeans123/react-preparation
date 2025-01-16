8041495ffd9bb985fd37758013e62e69d3f2d183
Kat <kat.m.deans@gmail.com>
2025-01-16 15:12:01 -0500

    Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react=router-dom'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+        <div className="App">
+              <Routes>
+                <Route></Route>
+                <Route></Route>
+              </Routes>
+        </div>
+    </Router>   
   );
 }
 
 export default App;
+
+
+// you need to wrap entire app in the browser router, and wrap all routse in <Routes> 
